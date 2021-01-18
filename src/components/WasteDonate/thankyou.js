ae580f92d365bfa1782d92a4ca59aeb1550a61db
prateekbose20011 <smitraprateek2001@gmail.com>
2021-01-18 20:22:06 +0530

    Update thankyou.js

--- src/components/WasteDonate/thankyou.js
+++ src/components/WasteDonate/thankyou.js
@@ -6,6 +6,18 @@ function Thankyou({setUserData, userData}) {
     const clickHandler = () => {
       setUserData({...userData});
       console.log(userData);
+      var xhr = new XMLHttpRequest();
+      var url = "http://localhost:3001/food";
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+              var json = JSON.parse(xhr.responseText);
+              console.log(json.email + ", " + json.password);
+          }
+      };
+      var data = JSON.stringify(userData);
+      xhr.send(data);
     }
 
     return (
