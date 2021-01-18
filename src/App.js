835f90b494cb1d7958cb1c8e1747a29b51dd2b2d
lawlesx <welcometohellv@gmail.com>
2021-01-18 19:10:11 +0530

    Added Wasted Food Section
    Added Complete Waste Food Section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route} from 'react-router-dom'
 import NavBar from './components/nav'
 import HeroSection from './components/hero'
 import Donatefood from './components/donatefood/donatefood'
+import DonateWaste from "./components/WasteDonate/donatewaste";
 
 function App() {
   return ([
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Route exact path='/' component={HeroSection} />
       <Route exact path='/donate' component={Donatefood} />
+      <Route exact path='/waste' component={DonateWaste} />
     </Router>
   ]);
 }
